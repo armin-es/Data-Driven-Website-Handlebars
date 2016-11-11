ea3c70817885de3639217f7e2f4d326a8d006228
Armin Eslami <armin.eslami@gmail.com>
2016-11-11 02:49:44 -0500

    feat: Add first level obejcts
    highest level data added (objects for countries)
    
    [ticket: #8]

--- app/js/data.js
+++ app/js/data.js
@@ -1,1 +1,16 @@
-// Data that are going to be replaced by Handlebars in HTML goes here
+// data that Handlebars template uses to fill the HTML file goes here
+
+var countries = [
+    {
+        name:"Canada",
+        thumbnail:"https://source.unsplash.com/_LuLiJc1cdo/400x225"
+    },
+    {
+        name:"Germany",
+        thumbnail:"https://source.unsplash.com/Mik1OWiveRs/400x225"
+    },
+    {
+        name:"Sweden",
+        thumbnail:"https://source.unsplash.com/xbkGzekJ-iM/400x225"
+    }
+];
