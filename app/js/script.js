7334b80fd80fbc5787bd55a746f5ade914251c35
Armin Eslami <armin.eslami@gmail.com>
2016-11-18 00:56:57 -0500

    feat: Execute the template
    Evaluate a Handlebars template and deliver to HTML
    
    [ticket: #8]

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
 var source = $("#countries-template").html();
 var countries_template = Handlebars.compile(source);
+var html = countries_template(data);
+$("#content").html(html);
 });
