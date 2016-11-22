9f5822522dc8141cdb47d7b9dd2556e256ea953d
Armin Eslami <armin.eslami@gmail.com>
2016-11-22 00:42:25 -0500

    feat: Add second-level data
    object properties with nested array values representing data for 'cities'
    
    [ticket: #8]

--- app/js/data.js
+++ app/js/data.js
@@ -1,16 +1,48 @@
 // data that Handlebars template uses to fill the HTML file goes here
 
-var countries = [
-    {
-        name:"Canada",
-        thumbnail:"https://source.unsplash.com/_LuLiJc1cdo/400x225"
-    },
-    {
-        name:"Germany",
-        thumbnail:"https://source.unsplash.com/Mik1OWiveRs/400x225"
-    },
-    {
-        name:"Sweden",
-        thumbnail:"https://source.unsplash.com/xbkGzekJ-iM/400x225"
-    }
-];
+var data = {
+    countries: [
+        {
+            name:"Canada",
+            thumbnail:"https://source.unsplash.com/_LuLiJc1cdo/400x225",
+            cities: [
+                {
+                    name:"Montreal",
+                    thumbnail:"https://source.unsplash.com/N6q6wtJ_mkA/800x450"
+                },
+                {
+                    name:"Toronto",
+                    thumbnail:"https://source.unsplash.com/LApT8J6e1Zo/800x450"
+                }
+            ]
+        },
+        {
+            name:"Germany",
+            thumbnail:"https://source.unsplash.com/Mik1OWiveRs/400x225",
+            cities: [
+                {
+                    name:"Berlin",
+                    thumbnail:"https://source.unsplash.com/n7JZs6Q1d1c/800x450"
+                },
+                {
+                    name:"Hamburg",
+                    thumbnail:"https://source.unsplash.com/QmemudqzrXQ/800x450"
+                }
+            ]
+        },
+        {
+            name:"Sweden",
+            thumbnail:"https://source.unsplash.com/xbkGzekJ-iM/400x225",
+            cities: [
+                {
+                    name:"Gothenburg",
+                    thumbnail:"https://source.unsplash.com/iYJuDqFMXHM/800x450"
+                },
+                {
+                    name:"Stockholm",
+                    thumbnail:"https://source.unsplash.com/qJtZOaaXSHU/800x450"
+                }
+            ]
+        }
+    ]
+};
