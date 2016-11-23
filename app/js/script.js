8ab8b655e302c08c0eda9a9cd25e1598c03402c0
Armin Eslami <armin.eslami@gmail.com>
2016-11-22 23:34:57 -0500

    feat: Create template function
    the function instantiate a template and displays the results in HTML content
    
    [ticket: #8]

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,9 @@
 var countries_template, cities_template;
 
+function useTemplate(template, data) {
+    var html = template(data);
+    $("content").html(html);
+}
 $(document).ready(function(){
     var source = $("#countries-template").html();
     countries_template = Handlebars.compile(source);
