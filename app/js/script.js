987ad5a6cfb759774990343b252990e1f893f904
Armin Eslami <armin.eslami@gmail.com>
2016-11-22 23:59:55 -0500

    fix: Correct 'useTemplate' function body
    the jQuery selector addressing content div was incorrect.
    
    [ticket: #8]

--- app/js/script.js
+++ app/js/script.js
@@ -2,11 +2,11 @@ var countries_template, cities_template;
 
 function useTemplate(template, data) {
     var html = template(data);
-    $("content").html(html);
+    $("#content").html(html);
 }
 $(document).ready(function(){
     var source = $("#countries-template").html();
     countries_template = Handlebars.compile(source);
-    var html = countries_template(data);
-    $("#content").html(html);
+    useTemplate(countries_template, data);
+
 });
