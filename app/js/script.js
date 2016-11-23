f5aa07e4e89dcbe6c6c932eacd29bd928f6f3150
Armin Eslami <armin.eslami@gmail.com>
2016-11-22 22:39:28 -0500

    style: Correct indentation
    [ticket: #8]

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-var source = $("#countries-template").html();
-var countries_template = Handlebars.compile(source);
-var html = countries_template(data);
-$("#content").html(html);
+    var source = $("#countries-template").html();
+    var countries_template = Handlebars.compile(source);
+    var html = countries_template(data);
+    $("#content").html(html);
 });
