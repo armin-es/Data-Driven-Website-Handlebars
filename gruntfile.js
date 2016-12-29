11df41c8e133b3f7c0813d020f01009d57b1a56d
Armin Eslami <armin.eslami@gmail.com>
2016-12-29 00:40:50 -0500

    fix: Update 'src' files in 'concat' task
    'handlebars.min.js', 'data.js' and 'script.js' files were added to be concatenated.
    
    [ticket: #9]

--- gruntfile.js
+++ gruntfile.js
@@ -13,58 +13,59 @@ module.exports = function(grunt) {
 
         concat: {
             js: {
-                src: ['node_modules/jquery/dist/jquery.js', 'node_modules/bootstrap/dist/js/bootstrap.js', 'app/js/app.js'],
-                dest: 'dist/js/script.min.js'
+                src: ['node_modules/jquery/dist/jquery.js', 'node_modules/bootstrap/dist/js/bootstrap.js', 'node_modules/handlebars/dist/handlebars.min.js', 'app/js/data.js', 'app/js/script.js'],
+                dest: 'dist/js/script.js'
             },
             css: {
-                src: ['app/css/style.css', 'node_modules/bootstrap/dist/css/bootstrap.css'],
-                dest: 'dist/css/style.min.css'
+                src: ['node_modules/bootstrap/dist/css/bootstrap.css', 'app/css/style.css'],
+                dest: 'dist/css/style.css'
             }
         },
 
-        uglify: {
-            ugly: {
-                src: 'dist/js/script.min.js',
-                dest: 'dist/js/script.min.js'
-            }
-        },
+        // uglify: {
+        //     ugly: {
+        //         src: 'dist/js/script.min.js',
+        //         dest: 'dist/js/script.min.js'
+        //     }
+        // },
 
-        cssmin: {
-            options: {
-                        shorthandCompacting: false,
-                        roundingPrecision: -1
-            },
-            mytarget: {
-                src:'dist/css/style.min.css',
-                dest: 'dist/css/style.min.css'
-            }
-        },
+        // cssmin: {
+        //     options: {
+        //                 shorthandCompacting: false,
+        //                 roundingPrecision: -1
+        //     },
+        //     mytarget: {
+        //         src:'dist/css/style.min.css',
+        //         dest: 'dist/css/style.min.css'
+        //     }
+        // },
 
-        processhtml: {
-            replaceScriptLinks: {
-                files: {
-                    './dist/index.html': ['./app/index.html']
-                }
-            }
-        },
+        // processhtml: {
+        //     replaceScriptLinks: {
+        //         files: {
+        //             './dist/index.html': ['./app/index.html']
+        //         }
+        //     }
+        // },
 
-        copy: {
-            main: {
-                cwd: 'app/img',
-                expand: true,
-                src:'*',
-                dest:'dist/img'
-            }
-        },
+        // copy: {
+        //     main: {
+        //         cwd: 'app/img',
+        //         expand: true,
+        //         src:'*',
+        //         dest:'dist/img'
+        //     }
+        // },
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-processhtml');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // grunt.loadNpmTasks('grunt-processhtml');
+    // grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin', 'processhtml', 'copy']);
+    grunt.registerTask('default', ['jshint', 'concat']);
+    // grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin', 'processhtml', 'copy']);
 
 };
